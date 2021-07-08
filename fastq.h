ffe2388e0ca7df20bed84c087e12179afc647acc
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-08 10:10:56 +0200

    Switch from tell64/seek64 to tell/seek functions.

--- fastq.h
+++ fastq.h
@@ -1,9 +1,6 @@
 #ifndef FASTQ_H
 #define FASTQ_H
 
-#define _LARGEFILE64_SOURCE
-#include <sys/types.h>
-#include <unistd.h>
 #include "constants.h"
 #include "parameters.h"
 #include "tree.h"
@@ -34,19 +31,19 @@ void closeFastqFiles (FILE **inputFastqFiles) {
  * Return false if we reached the EOF.
  * Should read the first 5 lines, because the first one could be truncated.
  */
-bool setToStart (FILE *inFile, off64_t posStart) {
+bool setToStart (FILE *inFile, off_t posStart) {
   char *line = NULL;
   size_t len = 0;
-  char    firstChars[2*FASTQ_LINES];
-  off64_t offsets[2*FASTQ_LINES-1];
+  char  firstChars[2*FASTQ_LINES];
+  off_t offsets[2*FASTQ_LINES-1];
   // Bad trick in case the 5th line is over the EOF.
   firstChars[2*FASTQ_LINES-1] = 0;
-  if (fseeko64(inFile, posStart, SEEK_SET) != 0) {
+  if (fseeko(inFile, posStart, SEEK_SET) != 0) {
     fprintf(stderr, "Error, problem reading FASTQ file.\nExiting.\n");
     exit(EXIT_FAILURE);
   }
   for (unsigned int lineId = 0; lineId < 2*FASTQ_LINES-1; ++lineId) {
-    offsets[lineId] = ftello64(inFile);
+    offsets[lineId] = ftello(inFile);
     if (getline(&line, &len, inFile) == -1) {
       // If the 5th line does not exist, it might still be ok...
       if (lineId == FASTQ_LINES) {
@@ -58,7 +55,7 @@ bool setToStart (FILE *inFile, off64_t posStart) {
     firstChars[lineId] = line[0];
     if (lineId >= 2) {
       if ((firstChars[lineId] == '+') && (firstChars[lineId-2] == '@')) {
-        if (fseeko64(inFile, offsets[lineId-2], SEEK_SET) != 0) {
+        if (fseeko(inFile, offsets[lineId-2], SEEK_SET) != 0) {
           fprintf(stderr, "Error, problem reading FASTQ file.\nExiting.\n");
           exit(EXIT_FAILURE);
         }
@@ -73,7 +70,7 @@ bool setToStart (FILE *inFile, off64_t posStart) {
   return false;
 }
 
-bool isPastEnd (off64_t posCurrent, off64_t posEnd) {
+bool isPastEnd (off_t posCurrent, off_t posEnd) {
   return (posCurrent > posEnd);
 }
 
@@ -82,7 +79,7 @@ bool isPastEnd (off64_t posCurrent, off64_t posEnd) {
  * Read a FASTQ file, and store it to a tree
  * Return false if we reach EOF
  */
-bool readReadsFile (FILE *inFile, char *fileName, tree_t *tree, unsigned int fileId, off64_t posStart, off64_t posEnd) {
+bool readReadsFile (FILE *inFile, char *fileName, tree_t *tree, unsigned int fileId, off_t posStart, off_t posEnd) {
   char *line = NULL;
   char *sequence = NULL;
   char *quality = NULL;
@@ -113,7 +110,7 @@ bool readReadsFile (FILE *inFile, char *fileName, tree_t *tree, unsigned int fil
     trimSequence(nRead, sequence);
     trimSequence(nRead, quality);
     addSequence(tree, nRead-1, sequence, quality, fileId);
-    over = isPastEnd(ftello64(inFile), posEnd);
+    over = isPastEnd(ftello(inFile), posEnd);
   }
   free(line);
   free(sequence);
